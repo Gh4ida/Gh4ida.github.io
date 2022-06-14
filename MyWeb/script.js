8216ef18372cfd286f484c723f0fafe67dc97e0d
Gh4ida <86244873+Gh4ida@users.noreply.github.com>
2022-06-14 23:30:17 +0300

    Add files via upload

--- MyWeb/script.js
+++ MyWeb/script.js
@@ -5,3 +5,14 @@ toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
 })
 
+// dark mode toggle
+var sunToggle=document.getElementById("sunToggle");
+
+sunToggle.onclick= function(){
+  document.body.classList.toggle("light-theme");
+  if(document.body.classList.contains("light-theme")){
+    sunToggle.src="images/moon.png";
+  }else{
+    sunToggle.src="images/sun.png";
+  }
+}
